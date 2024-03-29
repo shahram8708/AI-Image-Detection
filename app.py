629bdac51267ad238cefe86791860280f6bcaeba
shah.ram <ram.coding8@gmail.com>
2024-03-29 20:55:52 +0530

    first commit

--- app.py
+++ app.py
@@ -4,18 +4,10 @@ from werkzeug.utils import secure_filename
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import ResNet50, decode_predictions, preprocess_input
 import numpy as np
-from PIL import Image
-import io
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-model = ResNet50(weights='imagenet')
-
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+model = ResNet50(weights='imagenet') 
 
 @app.route('/')
 def index():
@@ -28,24 +20,20 @@ def upload_file():
     file = request.files['image']
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
-    if file and allowed_file(file.filename):
+    if file:
         filename = secure_filename(file.filename)
-        try:
-            img = Image.open(file.stream)
-            img = img.convert('RGB')
-            img = img.resize((224, 224), Image.NEAREST)
-            img_array = np.array(img)
-            img_array = np.expand_dims(img_array, axis=0)
-            processed_img = preprocess_input(img_array)
-
-            predictions = model.predict(processed_img)
-            result = decode_predictions(predictions, top=1)[0]
-
-            return jsonify({'name': str(result[0][1])})
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
-    else:
-        return jsonify({'error': 'File not allowed'}), 400
+        filepath = os.path.join('uploads', filename)
+        file.save(filepath)
+        
+        img = image.load_img(filepath, target_size=(224, 224))
+        img_array = image.img_to_array(img)
+        img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+        processed_img = preprocess_input(img_array_expanded_dims)
+        
+        predictions = model.predict(processed_img)
+        result = decode_predictions(predictions, top=1)[0]  
+        
+        return jsonify({'name': str(result[0][1])}) 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
